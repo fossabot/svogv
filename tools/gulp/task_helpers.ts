72d8aaf3a9086e6a6f23858447b62fa26367b8cc
Joerg Krause <joerg@krause.net>
2017-10-09 18:14:48 +0200

    css

--- tools/gulp/task_helpers.ts
+++ tools/gulp/task_helpers.ts
@@ -68,11 +68,16 @@ export function tsBuildTask(tsConfigPath: string, tsConfigName = 'tsconfig.json'
 
 /** Create a SASS Build Task. */
 export function sassBuildTask(dest: string, root: string) {
-  console.log('## SCSS Resolver path: ' + _globify(root, '**/*.scss') +  ' ==> ' + dest);
+  // console.log('## SCSS Resolver path: ' + _globify(root, '**/*.scss') +  ' ==> ' + dest);
   return () => {
     return gulp.src(_globify(root, '**/*.scss'))
       .pipe(gulpSourcemaps.init())
-      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(gulpSass({
+        includePaths: [
+          require('node-neat').includePaths,
+          require('include-media').includePath
+        ]
+      }).on('error', gulpSass.logError))
       .pipe(gulpAutoprefixer(SASS_AUTOPREFIXER_OPTIONS))
       .pipe(gulpSourcemaps.write('.'))
       .pipe(gulp.dest(dest));
